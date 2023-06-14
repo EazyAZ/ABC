4b32106a8504b38d9c68e1b795b0086a655471f7
Antoine Griffard <agriffard@hotmail.com>
2023-06-14 20:24:21 +0200

    wip

--- ABC.Business/_Imports.cs
+++ ABC.Business/_Imports.cs
@@ -3,5 +3,5 @@ global using ABC.Core.Enums;
 global using ABC.Business.Services;
 global using ABC.Core.Entities;
 global using ABC.Core.Interfaces.Business;
-global using ABC.Core.Interfaces.Data;
+//global using ABC.Core.Interfaces.Data;
 global using ABC.Core.Models.Reporting;
